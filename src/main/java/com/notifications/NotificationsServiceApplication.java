f822e62c810c051ef4c0476dbdcf13014533ed79
nahidanjumsheikh <103092810+nahidanjumsheikh@users.noreply.github.com>
2022-04-29 14:20:43 +0530

    Configured eureka client and added dependency of eureka client

--- src/main/java/com/notifications/NotificationsServiceApplication.java
+++ src/main/java/com/notifications/NotificationsServiceApplication.java
@@ -2,6 +2,7 @@ package com.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class NotificationsServiceApplication {
 
 	public static void main(String[] args) {
