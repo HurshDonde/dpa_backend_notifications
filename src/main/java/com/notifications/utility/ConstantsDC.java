13a6244012a85234dc88408ae5b6c9af842f0d6b
nahidanjumsheikh <103092810+nahidanjumsheikh@users.noreply.github.com>
2022-07-21 15:03:39 +0530

    added success and fail message in this file

--- src/main/java/com/notifications/utility/ConstantsDC.java
+++ src/main/java/com/notifications/utility/ConstantsDC.java
@@ -13,4 +13,7 @@ public class ConstantsDC {
 	public static final String EMAIL_SEND_FAIL = "Email sent fail";
 	public static final String SMS_SEND_SUCCESS = "SMS sent successfully";
 	public static final String SMS_SEND_FAIL = "SMS sent fail";
+	public static final String PUSH_NOTIFICATION_SEND_SUCCESSFULLY = "Push notification sent successfully";
+	public static final String PUSH_NOTIFICATION_SEND_FAIL = "Push notification sent fail";
+
 }
